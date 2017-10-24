471f8bad64c81e91088145158f9fa1baf88cc712
Hoon Lee <yl.hoony@gmail.com>
2017-10-24 17:14:05 +0000

    automatically backed up by learn

--- spec/valid_move_spec.rb
+++ spec/valid_move_spec.rb
@@ -1,5 +1,5 @@
 require_relative "../lib/valid_move.rb"
-
+ 
 describe './lib/valid_move.rb' do
   it 'returns true for a valid position on an empty board' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
